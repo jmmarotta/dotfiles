460506b5cc7bdd81c7496b753121c64298d8aba5
Julian Marotta <julianmmarotta@gmail.com>
2026-03-30 09:43:49 -0400

    nvim: rename diffview keymaps

--- nvim/.config/nvim/lua/plugins/diffview.lua
+++ nvim/.config/nvim/lua/plugins/diffview.lua
@@ -2,10 +2,10 @@ return {
   "sindrets/diffview.nvim",
   cmd = { "DiffviewOpen", "DiffviewFileHistory", "DiffviewClose" },
   keys = {
-    { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "[G]it [D]iffview" },
+    { "<leader>gdo", "<cmd>DiffviewOpen<cr>", desc = "[G]it [D]iffview [O]pen" },
     { "<leader>gh", "<cmd>DiffviewFileHistory %<cr>", desc = "[G]it File [H]istory" },
     { "<leader>gH", "<cmd>DiffviewFileHistory<cr>", desc = "[G]it Branch [H]istory" },
-    { "<leader>gc", "<cmd>DiffviewClose<cr>", desc = "[G]it Diffview [C]lose" },
+    { "<leader>gdc", "<cmd>DiffviewClose<cr>", desc = "[G]it [D]iffview [C]lose" },
   },
   opts = {},
 }
